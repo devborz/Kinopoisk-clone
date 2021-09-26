957441c47a1e92f55dd51407425279971e2a2e67
Usman Turkaev <u.turkaev@yandex.ru>
2021-09-26 18:40:00 +0300

    commit

---
+++ Kinopoisk/Kinopoisk/KinopoiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinopoiskApp.swift
+//  Kinopoisk
+//
+//  Created by Усман Туркаев on 26.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct KinopoiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
